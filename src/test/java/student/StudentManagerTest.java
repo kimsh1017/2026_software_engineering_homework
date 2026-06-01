e4c01b15dea6fa70b100ebbce57da262bc7800ab
Yujin Kim <127318207+sophia22001@users.noreply.github.com>
2026-06-01 15:18:30 +0900

    Feat/test2: beforeall 주석 추가  (#2)
    * test: given 주석 추가
    
    * feat: beforeall 주석 추가

--- src/test/java/student/StudentManagerTest.java
+++ src/test/java/student/StudentManagerTest.java
@@ -10,6 +10,8 @@ public class StudentManagerTest {
     private static StudentManager studentManager;
     private final static String testStudentName = "userA";
 
+    // 테스트 수행 전에 실행되어야합니다.
+    // 주석
     @BeforeAll
     static void beforeAll() {
         studentManager = new StudentManager();
